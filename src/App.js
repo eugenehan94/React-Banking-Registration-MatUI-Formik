7c47e04db3c9a35af0959e36dd7bdaf97055137c
eugenehan94 <eugene.han94@gmail.com>
2021-11-26 22:38:19 -0500

    first commit of project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  AppBar,
+  Box,
+  Button,
+  Grid,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import AccountBalanceIcon from "@material-ui/icons/AccountBalance";
+import backgroundVideo from "./Video/background-video.mp4";
+import { makeStyles } from "@material-ui/core/styles";
+import Footer from "./Components/Footer";
+const useStyles = makeStyles((theme) => ({
+  video: {
+    position: "fixed",
+    zIndex: "-1",
+    width: "100%",
+  },
+  actionMargin: {
+    marginTop: theme.spacing(8),
+    padding: theme.spacing(10),
+  },
+}));
 
-function App() {
+const App = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h4">Bank</Typography>
+          <AccountBalanceIcon fontSize="large" />
+        </Toolbar>
+      </AppBar>
+
+      <video autoPlay loop muted id="video" className={classes.video}>
+        <source src={backgroundVideo} type="video/mp4" />
+      </video>
+
+      <Box className={classes.actionMargin}>
+        <Grid container>
+          <Grid item xs={7}>
+            <Typography variant="h3" gutterBottom>
+              Already a MatUIBank Client
+            </Typography>
+            <hr />
+            <Box className={classes.lineMargin}>
+              <Typography variant="h6" gutterBottom>
+                Save time by signing onto MatUIBank Online or Mobile banking and
+                we'll complete some of the application for you.
+              </Typography>
+            </Box>
+            <Button>No, continue with application</Button>
+          </Grid>
+          <Grid item xs={5}></Grid>
+        </Grid>
+      </Box>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,13 @@
+
+
+import React from 'react'
+
+const index = () => {
+    return (
+        <div>
+            FOOTER
+        </div>
+    )
+}
+
+export default index
